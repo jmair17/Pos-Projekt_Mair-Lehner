6eec2bf5ac3e578aad741c684214bae31437256d
mlehner17 <lehner.maxi909@gmail.com>
2020-05-26 10:56:09 +0200

    Update

--- Reiseplaner/app/src/main/java/com/example/reiseplaner/MainActivity.java
+++ Reiseplaner/app/src/main/java/com/example/reiseplaner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.reiseplaner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        StartFragment startFragment = new StartFragment();
+        FragmentManager manager = getSupportFragmentManager();
+
+        manager.beginTransaction().add(R.id.mainLayout,startFragment).commit();
+
     }
 }
--- Reiseplaner/app/src/main/java/com/example/reiseplaner/StartFragment.java
+++ Reiseplaner/app/src/main/java/com/example/reiseplaner/StartFragment.java
@@ -4,10 +4,12 @@ package com.example.reiseplaner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -15,7 +17,7 @@ import android.view.ViewGroup;
  */
 public class StartFragment extends Fragment {
 
-
+    Button b1;
     public StartFragment() {
         // Required empty public constructor
     }
@@ -25,7 +27,20 @@ public class StartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View v = inflater.inflate(R.layout.fragment_start, container, false);
+
+        b1 = v.findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SecondFragment secondFragment = new SecondFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.mainLayout, secondFragment);
+                ft.commit();
+            }
+        });
+
+        return v;
     }
 
 }
