607bbd82a21e1173cfc7834acff3e2051fd1daad
jmair17 <55433990+jmair17@users.noreply.github.com>
2020-05-26 11:48:28 +0200

    Update

--- Reiseplaner/app/src/main/java/com/example/reiseplaner/fragment_start.java
+++ Reiseplaner/app/src/main/java/com/example/reiseplaner/fragment_start.java
@@ -4,16 +4,20 @@ package com.example.reiseplaner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class fragment_start extends Fragment {
+
+    Button b1;
 
 
     public fragment_start() {
@@ -25,7 +29,22 @@ public class fragment_start extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_start, container, false);
+        View v = inflater.inflate(R.layout.fragment_fragment_start, container, false);
+        b1 = v.findViewById(R.id.buttonStart);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                fragment_overview fragmentOverview = new fragment_overview();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                ft.replace(R.id.mainLayout, fragmentOverview);
+                ft.commit();
+            }
+        });
+
+
+        return v;
     }
 
 }
