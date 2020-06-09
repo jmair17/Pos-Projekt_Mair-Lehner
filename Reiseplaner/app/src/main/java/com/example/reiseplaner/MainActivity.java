75691bfeeac0ddebf7fc25bdcb210c54b3cd41d9
mlehner17 <lehner.maxi909@gmail.com>
2020-06-09 12:00:17 +0200

    addLayout

--- Reiseplaner/app/src/main/java/com/example/reiseplaner/MainActivity.java
+++ Reiseplaner/app/src/main/java/com/example/reiseplaner/MainActivity.java
@@ -28,19 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
     TextView editText;
 
-    ///////////////ADDING NEW JOURNEY////////////////
-    private EditText editTextCategory;
-    private EditText editTextDestination;
-    private EditText editTextJourneyDate;
-    private EditText editTextThingsNotToForget;
-    private EditText editTextNotes;
+
 
     ///////////////OTHERS////////////////////
     private MyAdapter<Journey> adapter;
-    private Button fab;
+
     Button buttonStart;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        StartFragment startFragment = new StartFragment();
+        FragmentManager manager = getSupportFragmentManager();
+
+        manager.beginTransaction().add(R.id.mainLayout,startFragment).commit();
+
+
+    }
 
     class Weather extends AsyncTask<String, Void, String> {
 
@@ -107,47 +115,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
 
-        StartFragment startFragment = new StartFragment();
-        FragmentManager manager = getSupportFragmentManager();
 
-        manager.beginTransaction().add(R.id.mainLayout,startFragment).commit();
 
-    }
-    public View addLayout(){
-        View vDialog = getLayoutInflater().inflate(R.layout.layout_newjourney, null);
-
-        editTextCategory = findViewById(R.id.editTextCategory);
-        editTextDestination = findViewById(R.id.editTextDestination);
-        editTextJourneyDate = findViewById(R.id.editTextJourneyDate);
-        editTextThingsNotToForget = findViewById(R.id.editTextThingsNotToForget);
-        editTextNotes = findViewById(R.id.editTextNotes);
-
-        return vDialog;
-    }
-
-    ///////ADDING A NEW JOURNEY///////////
-    public void addNewJourney(){
-        setContentView(R.layout.fragment_fragment_overview);
-        fab = findViewById(R.id.button2);
-
-        fab.setOnClickListener(v -> {
-
-            View vDialog = addLayout();
-            vDialog.findViewById(R.id.editTextCategory);
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("New Journey")
-                    .setView(vDialog)
-                    .setPositiveButton("Add", (dialog, which) ->{
-                        adapter.add(new Journey(editTextCategory.getText().toString(), editTextDestination.getText().toString(), editTextJourneyDate.getText().toString(), editTextThingsNotToForget.getText().toString(), editTextNotes.getText().toString()));
-                        adapter.notifyDataSetChanged();
-                    } ).setNegativeButton("Cancel", null)
-                    .show();
-        });
-    }
 }
--- Reiseplaner/app/src/main/java/com/example/reiseplaner/SecondFragment.java
+++ Reiseplaner/app/src/main/java/com/example/reiseplaner/SecondFragment.java
@@ -3,11 +3,15 @@ package com.example.reiseplaner;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
@@ -15,7 +19,13 @@ import android.view.ViewGroup;
  */
 public class SecondFragment extends Fragment {
 
-
+    private FloatingActionButton fab;
+    ///////////////ADDING NEW JOURNEY////////////////
+    private EditText editTextCategory;
+    private EditText editTextDestination;
+    private EditText editTextJourneyDate;
+    private EditText editTextThingsNotToForget;
+    private EditText editTextNotes;
     public SecondFragment() {
         // Required empty public constructor
     }
@@ -24,8 +34,34 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View v = inflater.inflate(R.layout.fragment_second, container, false);
+        fab = v.findViewById(R.id.floatingactionbutton);
+        editTextCategory = v.findViewById(R.id.editTextCategory);
+        editTextDestination = v.findViewById(R.id.editTextDestination);
+        editTextJourneyDate = v.findViewById(R.id.editTextJourneyDate);
+        editTextThingsNotToForget = v.findViewById(R.id.editTextThingsNotToForget);
+        editTextNotes = v.findViewById(R.id.editTextNotes);
+
+        fab.setOnClickListener(k -> {
+
+            View vDialog = v;
+            vDialog.findViewById(R.id.editTextCategory);
+            new AlertDialog.Builder(null)
+                    .setTitle("New Journey")
+                    .setView(vDialog)
+                    .setPositiveButton("Add", (dialog, which) ->{
+                        adapter.add(new Journey(editTextCategory.getText().toString(), editTextDestination.getText().toString(), editTextJourneyDate.getText().toString(), editTextThingsNotToForget.getText().toString(), editTextNotes.getText().toString()));
+                        adapter.notifyDataSetChanged();
+                    } ).setNegativeButton("Cancel", null)
+                    .show();
+        });
+        return v;
     }
+
+    ///////ADDING A NEW JOURNEY///////////
+    public void addNewJourney(){
+
+    }
+
 
 }
